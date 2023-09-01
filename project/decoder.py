cf628c9aa2d213f978734c37779828aa1a3232fa
nozwock <57829219+nozwock@users.noreply.github.com>
2023-09-01 19:17:25 +0530

    Rename function

--- project/decoder.py
+++ project/decoder.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 
-def image_to_text(img: np.ndarray, encoding: str = "utf-8") -> str:
+def decode_image_to_text(img: np.ndarray, encoding: str = "utf-8") -> str:
     """Converts a given image into the data associated with its pixel values"""
     return img.tobytes().decode(encoding=encoding)
