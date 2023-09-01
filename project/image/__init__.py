5d8d854d7f8d3710dcf08147356608356c9ceeda
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-09-01 20:57:53 +0300

    refactor: delete check for 3d array and refactor

--- project/image/__init__.py
+++ project/image/__init__.py
@@ -1,5 +1,9 @@
+import typing
+
 import numpy
-from project.image.encoders.encoder_interface import EncoderInterface
+
+from project.image.encoders.iface import EncoderInterface
+from pathlib import Path
 
 
 class Image:
@@ -8,26 +12,31 @@ class Image:
     pixel_array = None
 
     def __init__(self, pixel_array: numpy.ndarray):
-        if len(pixel_array.shape) != 3:
-            raise Exception(f"Unexpected pixel_arr shape: array has {len(pixel_array.shape)} dimensions, but expected 3")
+        # if len(pixel_array.shape) != 3:
+        #     raise Exception(f"Unexpected pixel_arr shape: array has {len(pixel_array.shape)} dimensions, but expected 3")
 
         self.pixel_array = pixel_array
         self.height = self.pixel_array.shape[0]
         self.width = self.pixel_array.shape[1]
         print("Created Instance")
 
-    def encode(self, encoder: EncoderInterface) -> None:
+    def encode(self, encoder: EncoderInterface) -> typing.Self:
         pass
 
-    def decode(self, encoder: EncoderInterface) -> None:
+    def decode(self, encoder: 'EncoderInterface') -> None:
         pass
 
 
 def read_image(filepath: str) -> Image:
-    if filepath == "":
+    pathlib_path = Path(filepath)
+    if not(pathlib_path.exists()):
         raise Exception(f"Invalid filepath provided: {filepath}")
 
     shape = (1, 1, 1)
     pixel_array = numpy.zeros(shape)
 
     return Image(pixel_array)
+
+
+if __name__ == "__main__":
+    image = read_image("./")
--- project/image/encoders/iface.py
+++ project/image/encoders/iface.py
@@ -1,7 +1,8 @@
 from typing import Protocol
+from project.image import Image
 
 
 class EncoderInterface(Protocol):
-    def encode(self) -> None: ...
+    def encode(self) -> Image: ...
 
     def decode(self) -> None: ...
