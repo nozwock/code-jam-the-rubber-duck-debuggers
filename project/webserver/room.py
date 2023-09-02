1ead84d196a2b048131a936df2bd48e206cfe656
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-09-02 23:01:27 +0300

    fix: linter error fix

--- project/webserver/room.py
+++ project/webserver/room.py
@@ -2,13 +2,15 @@ from enum import Enum, auto
 
 
 class RoomStatuses(Enum):
-    in_lobby = auto()  # waiting for people to join
-    playing = auto()  # playing main game
+    # waiting for people to join
+    in_lobby = auto()
+    # playing main game
+    playing = auto()
 
 
 class Room:
     """An object that handles one individual lobby and game."""
-    
+
     def __init__(self):
         self.status = RoomStatuses.in_lobby
 
@@ -21,4 +23,5 @@ class Room:
 
 class Classic(Room):
     """The default gamemode."""
+
     HTML_FILE = "classic.html"
