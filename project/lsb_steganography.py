47525598cd2728eb3e4ed0b2d0dfd95926a6cc21
nozwock <57829219+nozwock@users.noreply.github.com>
2023-09-02 21:56:30 +0530

    Fix LSB Steganography implementation

--- project/lsb_steganography.py
+++ project/lsb_steganography.py
@@ -1,44 +1,50 @@
 import numpy as np
+from bitarray import bitarray
 
 
-def encrypt(text: str, img: np.ndarray):
-    text += "#####"
-    text = "".join([f"{ord(i):08b}" for i in text])
-    total_pixels_required = (len(text) // 3)
-    bits_and_bobs = img.flatten()
-    for i in range(total_pixels_required):
-        if not bits_and_bobs[i] & int(text[i]):
-            bits_and_bobs |= int(text[i])
-    ret_img = bits_and_bobs.reshape(img.shape)
-    return ret_img
+def encode(
+    text: str, img: np.ndarray, encoding: str = "utf-8", message_end: bytes = b"\0"
+):
+    text += message_end.decode()
+    data = bitarray()
+    data.frombytes(bytes(text, encoding=encoding))
 
-    # for index, value in enumerate(img.flatten()):
-    #    print(something)
-    #    if index >= len(text):
-    #        something += (bytes(format(value, "08b"), "ascii"))
-    #        continue
-    #    something += bytes(format(value, "08b")[:-1] + text[index], "ascii")
-    # ret_img = np.frombuffer(something, dtype=img.dtype)
-    # ret_img = ret_img.reshape(img.shape)
-    # return ret_img
+    channels = img.flatten()
+    if len(data) > len(channels) * 8:
+        raise Exception(
+            "Can't fit the text within the image with the current implementation."
+        )
 
-
-def decrypt(img):
-    binary_values = list(map(lambda x: format(x, "08b")[-1], img.flatten()))
-    binary_values = ["".join(binary_values[i: i + 8]) for i in range(0, len(binary_values), 8)]
-    decoded_data = ""
-    for byte in binary_values:
-        decoded_data += chr(int(byte, 2))
-        if decoded_data[-5:] == "#####":
+    for i in range(channels.shape[0]):
+        try:
+            channels[i] = (channels[i] & ~1) | data.pop(0)
+        except IndexError:
             break
-    print(decoded_data[:-5])
-    return decoded_data[:-5]
+
+    return channels.reshape(img.shape)
+
+
+def decode(img: np.ndarray, encoding: str = "utf-8", message_end: bytes = b"\0") -> str:
+    channels = img.flatten()
+    data = bitarray()
+    for lv in channels:
+        data.append(lv & 1)
+
+        if (
+            len(data) % 8 == 0
+            and data[-len(message_end) * 8 :].tobytes() == message_end
+        ):
+            del data[-len(message_end) * 8 :]
+            break
+
+    return data.tobytes().decode(encoding=encoding)
 
 
 if __name__ == "__main__":
+    # Example
     import cv2 as cv
 
-    img = cv.imread("Untitled.png")
-    img_2 = encrypt("hello world", img)
-    cv.imwrite("output.png", encrypt("hello world", img))
-    decrypt(img_2)
+    img = cv.imread("image.png")
+    secret_img = encode("hello world", img)
+    cv.imwrite("output.png", secret_img)
+    print(decode(secret_img))
