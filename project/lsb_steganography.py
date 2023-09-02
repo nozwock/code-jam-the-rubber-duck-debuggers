ddbaa053e691f310768e2df27098fed4d7605570
realstealthninja <realstealthninja@gmail.com>
2023-09-02 21:59:00 +0530

    fix: make encode work

--- project/lsb_steganography.py
+++ project/lsb_steganography.py
@@ -2,30 +2,22 @@ import numpy as np
 
 
 def encrypt(text: str, img: np.ndarray):
-    text += "#####"
+    text += "#####"  # Delimiter for text change to your favorite character
     text = "".join([f"{ord(i):08b}" for i in text])
-    total_pixels_required = (len(text) // 3)
-    bits_and_bobs = img.flatten()
-    for i in range(total_pixels_required):
-        if not bits_and_bobs[i] & int(text[i]):
-            bits_and_bobs |= int(text[i])
-    ret_img = bits_and_bobs.reshape(img.shape)
-    return ret_img
 
-    # for index, value in enumerate(img.flatten()):
-    #    print(something)
-    #    if index >= len(text):
-    #        something += (bytes(format(value, "08b"), "ascii"))
-    #        continue
-    #    something += bytes(format(value, "08b")[:-1] + text[index], "ascii")
-    # ret_img = np.frombuffer(something, dtype=img.dtype)
-    # ret_img = ret_img.reshape(img.shape)
-    # return ret_img
+    flattened_img = img.flatten()
+    for i in range(len(text)):
+        flattened_img[i] = (flattened_img[i] & ~1) | int(text[i])
+
+    return flattened_img.reshape(img.shape)
 
 
 def decrypt(img):
     binary_values = list(map(lambda x: format(x, "08b")[-1], img.flatten()))
-    binary_values = ["".join(binary_values[i: i + 8]) for i in range(0, len(binary_values), 8)]
+    binary_values = [
+        "".join(binary_values[i : i + 8]) for i in range(0, len(binary_values), 8)
+    ]
+
     decoded_data = ""
     for byte in binary_values:
         decoded_data += chr(int(byte, 2))
