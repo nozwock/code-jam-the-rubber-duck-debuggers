23e483a04a85f3f7e37fe967c7b250441487af88
Sapient44 <doineedname18@gmail.com>
2023-09-03 16:41:48 +0530

    Added ceaser cipher encryption

--- project/cipher_choosing.py
+++ project/cipher_choosing.py
@@ -11,3 +11,28 @@ be chosen by the encrypter."""
     Vigenere = 4
     Playfair = 5
     Hill_Cipher = 6
+
+
+def encrypt_ceaser(text: str, password: int) -> str:
+    """Encrypts message using ceaser cipher"""
+    password = password % 26
+    cipher = ""
+    # A would be 3 = D
+    for letter in text:
+        unicode_letter = ord(letter)
+        if unicode_letter >= 65 and unicode_letter <= 90:  # Capital Letters
+            unicode_letter += password
+            if unicode_letter > 90:
+                # print(unicode_letter)
+                unicode_letter -= 26
+                # print(unicode_letter)
+        elif unicode_letter >= 97 and unicode_letter <= 122:  # Small Letters
+            unicode_letter += password
+            if unicode_letter > 122:
+                unicode_letter -= 26
+        cipher = cipher + chr(unicode_letter)
+    return (cipher)
+
+
+if __name__ == "__main__":
+    encrypt_ceaser("abcde vwxyz", 4)
