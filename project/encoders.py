ff2283e59792c1e4b32f8691841aca6e12df14ae
realstealthninja <realstealthninja@gmail.com>
2023-09-03 18:38:39 +0530

    feat: Add options to encode in cli

--- project/encoders.py
+++ project/encoders.py
@@ -2,6 +2,7 @@ import math
 
 import numpy as np
 from bitarray import bitarray
+from rich.progress import track
 
 from project.image import EncoderInterface, Image
 
@@ -78,7 +79,7 @@ class LsbSteganographyEncoder(EncoderInterface):
                 "Can't fit the data within the image with the current implementation."
             )
 
-        for i in range(channels.shape[0]):
+        for i in track(range(channels.shape[0]), description="Encoding to image..."):
             try:
                 channels[i] = (channels[i] & ~1) | data_bits.pop(0)
             except IndexError:
@@ -91,16 +92,16 @@ class LsbSteganographyEncoder(EncoderInterface):
         channels = img.flatten()
         bytes_for_data_bits = (len(channels).bit_length() + 7) // 8
         data_len_bits = bitarray()
-        for lv in channels[: bytes_for_data_bits * 8]:
+        for lv in track(channels[: bytes_for_data_bits * 8], description="Extracting bits from image..."):
             data_len_bits.append(lv & 1)
         data_len = int.from_bytes(
             data_len_bits.tobytes(), "big"
         )  # number of bytes in data
 
         data = bitarray()
-        for lv in channels[
+        for lv in track(channels[
             bytes_for_data_bits * 8 : bytes_for_data_bits * 8 + data_len * 8
-        ]:
+        ], description="Combining bits..."):
             data.append(lv & 1)
 
         return data.tobytes()
