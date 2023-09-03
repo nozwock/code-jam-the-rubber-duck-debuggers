057f41ccc80e0347d9956896e05d948513aaf9e2
Sapient44 <doineedname18@gmail.com>
2023-09-03 16:08:39 +0530

    Removed cryptography from dep

--- project/cipher_choosing.py
+++ project/cipher_choosing.py
@@ -5,10 +5,9 @@ class Cipher(Enum):
     """Adds the option of choosing the type of cipher, which would \
 be chosen by the encrypter."""
 
-    AES = 1
-    Ceaser = 2
-    Transposition = 3
-    Subsitution = 4
-    Vigenere = 5
-    Playfair = 6
-    Hill_Cipher = 7
+    Ceaser = 1
+    Transposition = 2
+    Subsitution = 3
+    Vigenere = 4
+    Playfair = 5
+    Hill_Cipher = 6
