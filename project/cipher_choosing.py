71efd12ac4b018d37f2d76f65bf5bc34ae7399b9
Sapient44 <doineedaname18@gmail.com>
2023-09-06 20:01:20 +0530

    Add chacha encryption

--- project/cipher_choosing.py
+++ project/cipher_choosing.py
@@ -99,130 +99,42 @@ class PBAESGCM(PBCipher):
         return cipher.decrypt(nonce, data, None)
 
 
-def deriveKey_pbkdf(password: str, salt: bytes | None = None) -> tuple[bytes, bytes]:
-    """Derives key for AES pbkdf"""
-    if salt is None:
-        salt = os.urandom(16)
-    return hashlib.pbkdf2_hmac("sha256", password.encode("utf8"), salt, 600_000), salt
+class PBChaCha(PBCipher):
+    """Password-based Chacha."""
 
+    SEP = b"$"
 
-def deriveKey_Argon(password: str, salt: bytes | None = None) -> tuple[bytes, bytes]:
-    """Derives key for Argon"""
-    if salt is None:
-        salt = os.urandom(16)
-    key = argon2.low_level.hash_secret_raw(
-        secret=bytes(password, "utf-8"),
-        salt=salt,
-        time_cost=argon2.DEFAULT_TIME_COST,
-        memory_cost=argon2.DEFAULT_MEMORY_COST,
-        parallelism=argon2.DEFAULT_PARALLELISM,
-        hash_len=argon2.DEFAULT_HASH_LENGTH,
-        type=argon2.Type.ID,
-    )
-    return key, salt
+    def encrypt(self, data: bytes, secret: bytes, kdf: KDF = Argon2()) -> bytes:
+        salt = kdf.salt
+        key = kdf.hash(secret)
+        cipher = cryptography_ciphers.ChaCha20Poly1305(key)
+        nonce = os.urandom(12)
 
+        encrypted_data = cipher.encrypt(nonce, data, None)
+        return (
+            base64.b64encode(salt)
+            + self.SEP
+            + base64.b64encode(nonce)
+            + self.SEP
+            + base64.b64encode(encrypted_data)
+        )
 
-def encrypt_aes_pbkdf(text: str, password: str) -> str:
-    """Encrypts message using AES cipher"""
-    key, salt = deriveKey_pbkdf(password)
-    aes = cryptography_ciphers.AESGCM(key)
-    nonce = os.urandom(12)
-    data = text.encode()
-    ciphertext = aes.encrypt(nonce, data, None)
-    return "{}${}${}".format(
-        base64.b64encode(salt).decode(),
-        base64.b64encode(nonce).decode(),
-        base64.b64encode(ciphertext).decode(),
-    )
+    def decrypt(self, data: bytes, secret: bytes, kdf: KDF = Argon2()) -> bytes:
+        salt, nonce, data = map(base64.b64decode, data.split(self.SEP))
+        assert len(nonce) == 12, "Corrupted encrypted data."
 
+        kdf.salt = salt
+        key = kdf.hash(secret)
+        cipher = cryptography_ciphers.ChaCha20Poly1305(key)
 
-def encrypt_aes_argon(text: str, password: str) -> bytes:
-    """Encrypts message using AES cipher"""
-    key, salt = deriveKey_Argon(password)
-    aes = cryptography_ciphers.AESGCM(key)
-    nonce = os.urandom(12)
-    data = text.encode()
-    ciphertext = aes.encrypt(nonce, data, None)
-    return "{}${}${}".format(
-        base64.b64encode(salt).decode(),
-        base64.b64encode(nonce).decode(),
-        base64.b64encode(ciphertext).decode(),
-    )
-
-
-def encrypt_chacha_pbkdf(text: str, password: str) -> bytes:
-    """Encrypts message using chacha cipher"""
-    key, salt = deriveKey_pbkdf(password)
-    text = bytes(text, "utf-8")
-    password = bytes(password, "utf-8")
-    chacha = cryptography_ciphers.ChaCha20Poly1305(key)
-    nonce = os.urandom(12)
-    ciphertext = chacha.encrypt(nonce, text, password)
-    return "{}${}${}".format(
-        base64.b64encode(salt).decode(),
-        base64.b64encode(nonce).decode(),
-        base64.b64encode(ciphertext).decode(),
-    )
-
-
-def encrypt_chacha_argon(text: str, password: str) -> bytes:
-    """Encrypts message using chacha cipher"""
-    key, salt = deriveKey_Argon(password)
-    text = bytes(text, "utf-8")
-    password = bytes(password, "utf-8")
-    chacha = cryptography_ciphers.ChaCha20Poly1305(key)
-    nonce = os.urandom(12)
-    ciphertext = chacha.encrypt(nonce, text, password)
-    return "{}${}${}".format(
-        base64.b64encode(salt).decode(),
-        base64.b64encode(nonce).decode(),
-        base64.b64encode(ciphertext).decode(),
-    )
-
-
-def decrypt_aes_pbkdf(cipher: str, password: str) -> str:
-    """Decrypts message using AES cipher"""
-    salt, nonce, cipher_data = map(base64.b64decode, cipher.split("$"))
-    key, _ = deriveKey_pbkdf(password, salt)
-    aes = cryptography_ciphers.AESGCM(key)
-    decrypted = aes.decrypt(nonce, cipher_data, None)
-    return decrypted.decode()
-
-
-def decrypt_aes_argon(cipher: str, password: str) -> str:
-    """Decrypts message using AES argon encryption"""
-    salt, nonce, cipher_data = map(base64.b64decode, cipher.split("$"))
-    key, _ = deriveKey_Argon(password, salt)
-    aes = cryptography_ciphers.AESGCM(key)
-    decrypted = aes.decrypt(nonce, cipher_data, None)
-    return decrypted.decode()
-
-
-def decrypt_chacha_pbkdf(cipher: str, password: str) -> str:
-    """Decrypts message using AES argon encryption"""
-    salt, nonce, cipher_data = map(base64.b64decode, cipher.split("$"))
-    key, _ = deriveKey_pbkdf(password, salt)
-    password = bytes(password, "utf-8")
-    chacha = cryptography_ciphers.ChaCha20Poly1305(key)
-    decrypted = chacha.decrypt(nonce, cipher_data, password)
-    return decrypted.decode()
-
-
-def decrypt_chacha_argon(cipher: str, password: str) -> str:
-    """Decrypts message using AES argon encryption"""
-    salt, nonce, cipher_data = map(base64.b64decode, cipher.split("$"))
-    key, _ = deriveKey_Argon(password, salt)
-    password = bytes(password, "utf-8")
-    chacha = cryptography_ciphers.ChaCha20Poly1305(key)
-    decrypted = chacha.decrypt(nonce, cipher_data, password)
-    return decrypted.decode()
+        return cipher.decrypt(nonce, data, None)
 
 
 if __name__ == "__main__":
     # cipher = encrypt_chacha_argon("duniya", "hello")
     # print(cipher)
     # print(decrypt_chacha_argon(cipher, "hello"))
-    cipher = PBAESGCM()
+    cipher = PBChaCha()
     encrypted = cipher.encrypt(b"hello world", b"1234")
     decrypted = cipher.decrypt(encrypted, b"1234")
     print(f"{decrypted, encrypted=}")
