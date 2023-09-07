9ddcabb34e335c8c1e716694486d95c257e1087b
realstealthninja <realstealthninja@gmail.com>
2023-09-07 20:48:42 +0530

    fix: make text thingy fully inpaint the words

--- project/text_thingies.py
+++ project/text_thingies.py
@@ -1,4 +1,5 @@
 import pytesseract
+from pytesseract import Output
 import cv2 as cv
 import numpy as np
 from pathlib import Path
@@ -9,18 +10,20 @@ img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 img_cpy = img.copy()
 height, width, channels = img.shape
 
-boxes = pytesseract.image_to_boxes(img_rgb).splitlines()
-for box in boxes:
-    x1, y1, x2, y2 = map(int, box.split(" ")[1:-1])
-    img_cpy = cv.rectangle(img, (x1, height - y1), (x2, height - y2), (0, 255, 0), 2)
+d = pytesseract.image_to_data(img_rgb, output_type=Output.DICT)
+n_boxes = len(d["text"])
+for i in range(n_boxes):
+    if int(d["conf"][i]) < 60:
+        continue
+
+    (x, y, w, h) = (d["left"][i], d["top"][i], d["width"][i], d["height"][i])
     mask = np.zeros(img.shape[:2], np.uint8)
-    mask[y1:y2, x1:x2] = 255
+    mask[y : y + h, x : x + w] = 255
     masked_img = cv.bitwise_and(img, img, mask=mask)
-    cv.imshow("", masked_img)
+    cv.imshow("", mask)
     cv.waitKey(0)
     img = cv.inpaint(img, mask, 3, cv.INPAINT_NS)
 
-cv.imshow("", img_cpy)
 cv.waitKey(0)
 cv.imshow("", img)
 cv.waitKey(0)
