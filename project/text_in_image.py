ba48552e6793a2897e2b224196f46ebfb9459aa7
Sapient44 <doineedaname18@gmail.com>
2023-09-07 21:36:39 +0530

    Add text in image

--- project/text_in_image.py
+++ project/text_in_image.py
@@ -8,5 +8,18 @@ image[:, :, 0] = b
 image[:, :, 1] = g
 image[:, :, 2] = r
 
+# font
+font = cv2.FONT_HERSHEY_SIMPLEX
+# origin
+origin = (0, 200)
+# fontScale
+fontScale = 1
+# Blue color in BGR
+color = (255, 0, 0)
+# Line thickness of 2 px
+thickness = 2
+# Using cv2.putText() method
+image = cv2.putText(image, 'OpenCV', origin, font,
+                    fontScale, color, thickness, cv2.LINE_AA)
 cv2.imshow("A New Image", image)
 cv2.waitKey(0)
