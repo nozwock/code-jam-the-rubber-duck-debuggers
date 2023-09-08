8f3450e7fb1694060fadb634af1030ddaf227135
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-09-08 18:28:38 +0300

    fix: create filled button component

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Filled_button from "../ui/Filled_button.svelte";
+
+    function onButtonClick() {
+        console.log("button clicked")
+    }
+</script>
+
+<!--<h1>Welcome to SvelteKit</h1>-->
+<!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
+<Filled_button text="text" onClicked={onButtonClick}/>
---
+++ client/src/ui/Filled_button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+   export let text: string;
+   export let onClicked: () => void;
+</script>
+
+<style>
+   .filledButton {
+      background-color: #1c92ff;
+      border-radius: 15px;
+      padding: 15px;
+      color: white;
+      font-size: 18px;
+      font-weight: bold;
+      border: none;
+   }
+</style>
+
+<button class="filledButton" on:click={onClicked}>
+   {text}
+</button>
+
+
