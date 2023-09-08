43d091cd09a21ab8a4bc784125a60596e686c358
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-09-08 19:02:18 +0300

    fix: create input component

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,11 +1,20 @@
-<script>
+<script lang="ts">
     import Filled_button from "../ui/Filled_button.svelte";
+    import Custom_Input from "../ui/Custom_Input.svelte";
+
+    let text = "";
 
     function onButtonClick() {
-        console.log("button clicked")
+        console.log("button clicked");
+    }
+
+    function onSubmitInput(text: string){
+        console.log(text)
     }
 </script>
 
 <!--<h1>Welcome to SvelteKit</h1>-->
 <!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
+
+<Custom_Input text={text} placeholder="PLACEHOLDER" onSubmit={onSubmitInput}/>
 <Filled_button text="text" onClicked={onButtonClick}/>
---
+++ client/src/ui/Custom_Input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    type OnSubmitFunction= (text: string) => void
+
+    export let text: string;
+    export let placeholder: string;
+    export let onSubmit:OnSubmitFunction = (text: string)=>null;
+    
+    function onKeyDown(e:KeyboardEvent) {
+        if (e.key == "Enter"){
+            onSubmit(text)
+        }
+    }
+</script>
+
+<style>
+    .input{
+        border-radius: 10px;
+        padding: 15px;
+        font-size: 16px;
+    }
+</style>
+
+<input class="input" bind:value={text} placeholder={placeholder} on:keydown={onKeyDown}/>
