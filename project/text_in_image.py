74877cf69bcea7c49513fcd3c9121684bb792de5
Sapient44 <doineedaname18@gmail.com>
2023-09-08 11:11:55 +0530

    Add mutliple text in the image

--- project/text_in_image.py
+++ project/text_in_image.py
@@ -11,15 +11,21 @@ image[:, :, 2] = r
 # font
 font = cv2.FONT_HERSHEY_SIMPLEX
 # origin
-origin = (0, 200)
-# fontScale
-fontScale = 1
-# Blue color in BGR
-color = (255, 0, 0)
-# Line thickness of 2 px
-thickness = 2
-# Using cv2.putText() method
-image = cv2.putText(image, 'OpenCV', origin, font,
-                    fontScale, color, thickness, cv2.LINE_AA)
+x, y = 0, 0
+while y < height:
+    while x < width:
+        origin = (x, y)
+        # fontScale
+        fontScale = 1
+        # Blue color in BGR
+        color = (255, 0, 0)
+        # Line thickness of 2 px
+        thickness = 2
+        # Using cv2.putText() method
+        image = cv2.putText(image, 'OpenCV', origin, font,
+                            fontScale, color, thickness, cv2.LINE_AA)
+        x += 200
+    y += 50
+    x = 0
 cv2.imshow("A New Image", image)
-cv2.waitKey(0)
+cv2.waitKey(2000)
